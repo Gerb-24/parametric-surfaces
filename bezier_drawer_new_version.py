f24441b9b4d316cadc34013febf304984d8bb394
Gerb-24 <61150608+Gerb-24@users.noreply.github.com>
2020-03-07 23:41:09 +0100

    plot works now
    The UI works and the plot is working as well, it also updates when pressing a number

--- bezier_drawer_new_version.py
+++ bezier_drawer_new_version.py
@@ -30,10 +30,9 @@ class PlotCanvas(FigureCanvas):
 
     def __init__(self, parent=None, width=8, height=8, dpi=100):
         self._figure = Figure(figsize=(width, height), dpi=dpi)
-        # self._axes = self._figure.add_subplot(111)
 
-        FigureCanvas.__init__(self, self._figure)
-        self.setParent(parent)
+        FigureCanvas.__init__(self, self._figure) # this gives the init from the super class FigureCanvas
+        self.setParent(parent) # it has parent as parent, we want to give the mainwindow as parent later on
 
         FigureCanvas.setSizePolicy(self,
                 QSizePolicy.Expanding,
@@ -149,12 +148,6 @@ class DraggablePlotExample(PlotCanvas):
             self.update()
             """update updates the whole GUI, which also runs the bezier plots with the new pointlist"""
             self._update_plot()
-        # # right click
-        # elif event.button == 3 and event.inaxes in [self._axes]:
-        #     point = self._find_neighbor_point(event)
-        #     if point:
-        #         self._remove_point(*point)
-        #         self._update_plot()
 
     def _on_release(self, event):
         u""" callback method for mouse release event
@@ -273,179 +266,31 @@ class AppForm(QMainWindow):
         super().__init__()
         self.left = 400
         self.top = 400
-        self.title = 'b-curve draw window'
         self.width = 1000
         self.height = 800
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        # it will start drawing the mainwindow at 400 400 with a width of 1000 and a height of 800
+
+        self.title = 'b-curve draw window'
+        self.setWindowTitle(self.title)
+
         self.initUI()
+        # in here we put all the buttons and other UI elements
 
 
     def initUI(self):
-        # self.create_menu()
-        # self.create_main_frame()
-        # self.create_status_bar()
         #plot 2d graph
         self.plot = DraggablePlotExample(self, width=8, height=8)
         self.plot.move(0,0)
         #plot 3d graph
         #self.plot2 = Surface3D(self, width=5, height=4)
         #self.plot2.move(600,0)
-        self.setGeometry(self.left, self.top, self.width, self.height)
+        self.Ui_MainWindow()
+        self.retranslateUi()
 
-    def on_about(self):
-        msg = """ A demo of using PyQt with matplotlib:
-
-         * Use the matplotlib navigation bar
-         * Add values to the text box and press Enter (or click "Draw")
-         * Show or hide the grid
-         * Drag the slider to modify the width of the bars
-         * Save the plot to a file using the File menu
-         * Click on a bar to receive an informative message
-        """
-        QMessageBox.about(self, "About the demo", msg.strip())
-
-
-    def on_draw(self):
-        """ Redraws the figure
-        """
-        self.updateplot1()
-        self.updateplot2()
-
-    def updateplot1(self):
-        self.plot._update_plot
-
-    def updateplot2(self):
-        self.plot2.update_plot(self.plot.x,self.plot.y,graph_depth=self.slider.value())
-
-
-    def create_main_frame(self):
-        self.main_frame = QWidget()
-
-        # Create the mpl Figure and FigCanvas objects.
-        # 5x4 inches, 100 dots-per-inch
-        #
-        self.dpi = 100
-        self.fig = Figure((5.0, 4.0), dpi=self.dpi)
-        self.canvas = FigureCanvas(self.fig)
-        self.canvas.setParent(self.main_frame)
-
-        # Since we have only one plot, we can use add_axes
-        # instead of add_subplot, but then the subplot
-        # configuration tool in the navigation toolbar wouldn't
-        # work.
-        #
-#        self.axes = self.fig.add_subplot(111)
-
-        # Bind the 'pick' event for clicking on one of the bars
-        #
-
-        # Create the navigation toolbar, tied to the canvas
-        #
-        self.mpl_toolbar = NavigationToolbar(self.canvas, self.main_frame)
-
-        # Other GUI controls
-        #
-        self.draw_button = QPushButton("&Draw 3D")
-        self.draw_button.clicked.connect(self.updateplot2)
-
-
-        slider_label = QLabel('3D depth (u):')
-        self.slider = QSlider(Qt.Horizontal)
-        self.slider.setRange(1, 100)
-        self.slider.setValue(20)
-        self.slider.setTracking(True)
-        self.slider.setTickPosition(QSlider.TicksBothSides)
-        self.slider.valueChanged.connect(self.on_draw)
-
-        #
-        # Layout with box sizers
-        #
-        hbox = QHBoxLayout()
-
-        for w in [  self.draw_button, slider_label, self.slider]:
-            hbox.addWidget(w)
-            hbox.setAlignment(w, Qt.AlignVCenter)
-
-        vbox = QVBoxLayout()
-        vbox.addWidget(self.canvas)
-        vbox.addWidget(self.mpl_toolbar)
-        vbox.addLayout(hbox)
-
-        self.main_frame.setLayout(vbox)
-        self.setCentralWidget(self.main_frame)
-
-    def create_status_bar(self):
-        self.status_text = QLabel("This is a demo")
-        self.statusBar().addWidget(self.status_text, 1)
-
-    def create_menu(self):
-        self.file_menu = self.menuBar().addMenu("&File")
-#
-#        load_file_action = self.create_action("&Save plot",
-#            shortcut="Ctrl+S", slot=self.save_plot,
-#            tip="Save the plot")
-#        quit_action = self.create_action("&Quit", slot=self.close,
-#            shortcut="Ctrl+Q", tip="Close the application")
-
-        self.help_menu = self.menuBar().addMenu("&Help")
-        about_action = self.create_action("&About",
-            shortcut='F1', slot=self.on_about,
-            tip='About the demo')
-
-        self.add_actions(self.help_menu, (about_action,))
-
-    def add_actions(self, target, actions):
-        for action in actions:
-            if action is None:
-                target.addSeparator()
-            else:
-                target.addAction(action)
-
-    def create_action(  self, text, slot=None, shortcut=None,
-                        icon=None, tip=None, checkable=False):
-        action = QAction(text, self)
-        if icon is not None:
-            action.setIcon(QIcon(":/%s.png" % icon))
-        if shortcut is not None:
-            action.setShortcut(shortcut)
-        if tip is not None:
-            action.setToolTip(tip)
-            action.setStatusTip(tip)
-        if slot is not None:
-            action.triggered.connect(slot)
-        if checkable:
-            action.setCheckable(True)
-        return action
-
-    def keyPressEvent(self, e):
-        # =============================================================================
-        #create dictionary for keys in text
-        # curvekeys = {}
-        # Numberedkeys = 9
-        # for i in range(Numberedkeys):
-        #     curvekeys[i+1]=('Qt.Key_{}'.format(i+1))
-        #
-        # print(curvekeys)
-        # =============================================================================
-        curvekeys = {1: Qt.Key_1, 2: Qt.Key_2, 3: Qt.Key_3, 4: Qt.Key_4, 5: Qt.Key_5, 6: Qt.Key_6, 7: Qt.Key_7, 8: Qt.Key_8, 9: Qt.Key_9}
-        #create dictionary for keys
-
-        length = len(self.plot.pointlist)
-        for i,arg in curvekeys.items():
-            """this will select curve i in curve mode"""
-            if e.key() == arg:
-                    if i <= length:
-                        self.plot.selected_curve = i
-                        self.plot.update()
-                        self.updateplot1
-
-class Ui_MainWindow(object):
-    def setupUi(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(1000, 800)
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
-        self.centralwidget.setObjectName("centralwidget")
-        self.groupBox_2 = QtWidgets.QGroupBox(self.centralwidget)
-        self.groupBox_2.setGeometry(QtCore.QRect(800, 0, 200, 800))
+    def Ui_MainWindow(self):
+        self.groupBox_2 = QtWidgets.QGroupBox(self)
+        self.groupBox_2.setGeometry(QtCore.QRect(800, 10, 200, 800))
         self.groupBox_2.setFlat(False)
         self.groupBox_2.setCheckable(False)
         self.groupBox_2.setObjectName("groupBox_2")
@@ -514,33 +359,29 @@ class Ui_MainWindow(object):
         self.lineEdit_6 = QtWidgets.QLineEdit(self.groupBox_5)
         self.lineEdit_6.setObjectName("lineEdit_6")
         self.formLayout_2.setWidget(2, QtWidgets.QFormLayout.FieldRole, self.lineEdit_6)
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar = QtWidgets.QMenuBar(self)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 1000, 26))
         self.menubar.setObjectName("menubar")
         self.menuFile = QtWidgets.QMenu(self.menubar)
         self.menuFile.setObjectName("menuFile")
-        MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(self)
         self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
-        self.actionNew = QtWidgets.QAction(MainWindow)
+        self.setStatusBar(self.statusbar)
+        self.actionNew = QtWidgets.QAction(self)
         self.actionNew.setObjectName("actionNew")
-        self.actionLoad = QtWidgets.QAction(MainWindow)
+        self.actionLoad = QtWidgets.QAction(self)
         self.actionLoad.setObjectName("actionLoad")
-        self.actionSave = QtWidgets.QAction(MainWindow)
+        self.actionSave = QtWidgets.QAction(self)
         self.actionSave.setObjectName("actionSave")
         self.menuFile.addAction(self.actionNew)
         self.menuFile.addAction(self.actionLoad)
         self.menuFile.addAction(self.actionSave)
         self.menubar.addAction(self.menuFile.menuAction())
 
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
-
-    def retranslateUi(self, MainWindow):
+    def retranslateUi(self):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.setWindowTitle(_translate("MainWindow", "MainWindow"))
         self.groupBox_2.setTitle(_translate("MainWindow", "Tools"))
         self.groupBox_3.setTitle(_translate("MainWindow", "Curvepart tools"))
         self.pushButton.setText(_translate("MainWindow", "add curvepart"))
@@ -559,11 +400,44 @@ class Ui_MainWindow(object):
         self.actionLoad.setText(_translate("MainWindow", "Load"))
         self.actionSave.setText(_translate("MainWindow", "Save"))
 
+    def updateplot1(self):
+        self.plot._update_plot()
+
+    def updateplot2(self):
+        self.plot2.update_plot(self.plot.x,self.plot.y,graph_depth=self.slider.value())
+
+
+    def keyPressEvent(self, e):
+        # =============================================================================
+        #create dictionary for keys in text
+        # curvekeys = {}
+        # Numberedkeys = 9
+        # for i in range(Numberedkeys):
+        #     curvekeys[i+1]=('Qt.Key_{}'.format(i+1))
+        #
+        # print(curvekeys)
+        # =============================================================================
+        curvekeys = {1: Qt.Key_1, 2: Qt.Key_2, 3: Qt.Key_3, 4: Qt.Key_4, 5: Qt.Key_5, 6: Qt.Key_6, 7: Qt.Key_7, 8: Qt.Key_8, 9: Qt.Key_9}
+        #create dictionary for keys
+
+        length = len(self.plot.pointlist)
+        for i,arg in curvekeys.items():
+            """this will select curve i in curve mode"""
+            if e.key() == arg:
+                    if i <= length:
+                        self.plot.selected_curve = i
+                        self.plot.update()
+                        self.updateplot1()
+
+
+
+
+
 def main():
     app = QApplication(sys.argv)
     form = AppForm()
-    ui = Ui_MainWindow()
-    ui.setupUi(form)
+    # ui = Ui_MainWindow()
+    # ui.setupUi(form)
     form.show()
     sys.exit(app.exec_())
 
