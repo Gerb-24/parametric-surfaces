a5d1a41668dc82b41f0c263d43a3bb7abdf91251
Gerb-24 <61150608+Gerb-24@users.noreply.github.com>
2020-03-11 13:24:40 +0100

    saving and loading updated

--- file_menu_functions.py
+++ file_menu_functions.py
@@ -15,11 +15,27 @@ def load(self):
         return
     with open(filepath, "r") as text:
         self.plot.pointlist = ast.literal_eval(text.readline())
+
+        # updating self.pointdict
+        self.plot.pointdict = []
+        self.plot.pointdict.append({"node": self.plot.pointlist[0][0], "handles": [self.plot.pointlist[0][1]]})
+        for i in range(len(self.plot.pointlist)-1):
+            self.plot.pointdict.append({"node": self.plot.pointlist[i][3], "handles": [self.plot.pointlist[i][2],self.plot.pointlist[i+1][1]]})
+        self.plot.pointdict.append({"node": self.plot.pointlist[len(self.plot.pointlist)-1][3], "handles": [self.plot.pointlist[len(self.plot.pointlist)-1][2]]})
+
         self.plot.update()
         self.plot._update_plot()
         text.close()
 
 def newfile(self):
     self.plot.pointlist = [[[100,250],[200,250],[300,250],[400,250]]]
+
+    # updating self.pointdict
+    self.plot.pointdict = []
+    self.plot.pointdict.append({"node": self.plot.pointlist[0][0], "handles": [self.plot.pointlist[0][1]]})
+    for i in range(len(self.plot.pointlist)-1):
+        self.plot.pointdict.append({"node": self.plot.pointlist[i][3], "handles": [self.plot.pointlist[i][2],self.plot.pointlist[i+1][1]]})
+    self.plot.pointdict.append({"node": self.plot.pointlist[len(self.plot.pointlist)-1][3], "handles": [self.plot.pointlist[len(self.plot.pointlist)-1][2]]})
+    
     self.plot.update()
     self.plot._update_plot()
